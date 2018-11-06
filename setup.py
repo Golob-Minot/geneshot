74172db896702bad0b0fa1969dbcbcf744cd26ab
Jonathan Golob <jonathan@golob.org>
2018-11-06 11:50:02 -0500

    Working towards packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="geneshot",
+    version="0.0.1",
+    author="Jonathan Golob",
+    author_email="j-dev@golob.org",
+    description="A gene-level metagenomics pipeline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jgolob/geneshot",
+    packages=setuptools.find_packages(),
+    dependency_links=[
+        'https://github.com/jgolob/sciluigi/tarball/containertask',
+    ],
+    install_requires=[
+        
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
