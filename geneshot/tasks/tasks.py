5e288a36fb14e1822d66cb032dd9230afcffbecb
Jonathan Golob <jgolob@rhino3.fhcrc.org>
2018-11-20 14:54:28 -0800

    Bug fix combine reads

--- geneshot/tasks/tasks.py
+++ geneshot/tasks/tasks.py
@@ -374,7 +374,7 @@ class CombineReads(sl.ContainerTask):
         }
 
     def run(self):
-        assert len(self.in_reads_list) > 2, "Less than two read pairs to combine. Nothing to do"
+        assert len(self.in_reads_list) >= 2, "Less than two read pairs to combine. Nothing to do"
         # Implicit else
         input_targets = {}
         R1_command_string = ""
@@ -382,19 +382,23 @@ class CombineReads(sl.ContainerTask):
         for index, read_pair in enumerate(self.in_reads_list):
             input_targets['p{}_R1'.format(index)] = read_pair()["R1"]
             input_targets['p{}_R2'.format(index)] = read_pair()["R2"]
-            R1_command_string += 'p{}_R1'.format(index)
-            R2_command_string += 'p{}_R2'.format(index)
+            R1_command_string += '$p{}_R1 '.format(index)
+            R2_command_string += '$p{}_R2 '.format(index)
+        command = (
+            'combine_fasta.py '
+            '{} '
+            '-q '
+            '-o $out_R1 && '
+            'combine_fasta.py '
+            '{} '
+            '-q '
+            '-o $out_R2'
+        ).format(
+            R1_command_string,
+            R2_command_string
+        )
         self.ex(
-            command=(
-                'combine_fasta.py '
-                '$R1_string '
-                '-q '
-                '-o $out_R1 &&'
-                'combine_fasta.py '
-                '$R2_string '
-                '-q '
-                '-o $out_R2'
-            ),
+            command=command,
             input_targets=input_targets,
             output_targets={
                 'out_R1': self.out_reads()['R1'],
@@ -405,7 +409,7 @@ class CombineReads(sl.ContainerTask):
                 'R2_string': R2_command_string,
             }
         )
-        
+
 
 class MetaSPAdesAssembly(sl.ContainerTask):
     # runMetaSpades
