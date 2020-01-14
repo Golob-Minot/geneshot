f54ce2c8e3aa9e901780d57ec59a3fab72f0d7d4
Sam Minot <sam@fredhutch.org>
2020-01-14 10:46:28 -0800

    Test the workflow with and without preprocessing

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Run this script to test geneshot on the example data in the repository
 # All execution will be done locally using Docker, so there is no need to
 # set up HPC or cloud execution configurations
@@ -12,6 +14,17 @@
 NXF_VER=19.10.0 nextflow run main.nf \
     -c nextflow.config \
     -profile testing \
+    --manifest data/mock.manifest.csv \
+    --preprocess_output output/preprocess_output \
+    --output output/ \
+    --hg_index data/hg_chr_21_bwa_index.tar.gz \
+    -w work/ \
+    -resume
+
+NXF_VER=19.10.0 nextflow run main.nf \
+    -c nextflow.config \
+    -profile testing \
+    --nopreprocess \
     --manifest data/mock.manifest.csv \
     --output output/ \
     --hg_index data/hg_chr_21_bwa_index.tar.gz \
