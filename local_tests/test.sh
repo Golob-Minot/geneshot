dd7694c08077e8dad07ae4c063bf84c01e5bbb40
Sam Minot <sam@fredhutch.org>
2020-02-28 14:23:59 -0800

    Added test script to help with merge.

--- local_tests/test.sh
+++ local_tests/test.sh
@@ -15,100 +15,114 @@ set -e
 NXF_VER=19.10.0 nextflow run main.nf \
     -c nextflow.config \
     -profile testing \
-    --manifest data/mock.manifest.csv \
-    --preprocess_output output/preprocess_output \
-    --output output1 \
+    --manifest data/mock.manifest.2.csv \
+    --nopreprocess \
+    --output output0 \
     --hg_index data/hg_chr_21_bwa_index.tar.gz \
     --formula "label1 + label2" \
-    --distance_threshold 0.1 \
-    -w work/ \
-    --noannot \
-    --savereads \
-    -resume
-
-# Test with preprocessing and no formula
-NXF_VER=19.10.0 nextflow run main.nf \
-    -c nextflow.config \
-    -profile testing \
-    --manifest data/mock.manifest.csv \
-    --preprocess_output output/preprocess_output \
-    --output output2 \
-    --hg_index data/hg_chr_21_bwa_index.tar.gz \
-    --distance_threshold 0.1 \
+    --distance_threshold 0.5 \
     -w work/ \
     --noannot \
     -resume
 
-# Test with formula and no preprocessing
-NXF_VER=19.10.0 nextflow run main.nf \
-    -c nextflow.config \
-    -profile testing \
-    --nopreprocess \
-    --manifest data/mock.manifest.csv \
-    --output output3 \
-    --hg_index data/hg_chr_21_bwa_index.tar.gz \
-    --formula "label1 + label2" \
-    --distance_threshold 0.1 \
-    -w work/ \
-    --noannot \
-    -resume
+# # Test with preprocessing and a formula
+# NXF_VER=19.10.0 nextflow run main.nf \
+#     -c nextflow.config \
+#     -profile testing \
+#     --manifest data/mock.manifest.csv \
+#     --preprocess_output output/preprocess_output \
+#     --output output1 \
+#     --hg_index data/hg_chr_21_bwa_index.tar.gz \
+#     --formula "label1 + label2" \
+#     --distance_threshold 0.1 \
+#     -w work/ \
+#     --noannot \
+#     --savereads \
+#     -resume
 
-# Test with no formula and no preprocessing
-NXF_VER=19.10.0 nextflow run main.nf \
-    -c nextflow.config \
-    -profile testing \
-    --nopreprocess \
-    --manifest data/mock.manifest.csv \
-    --output output4 \
-    --hg_index data/hg_chr_21_bwa_index.tar.gz \
-    -w work/ \
-    --noannot \
-    -resume
+# # Test with preprocessing and no formula
+# NXF_VER=19.10.0 nextflow run main.nf \
+#     -c nextflow.config \
+#     -profile testing \
+#     --manifest data/mock.manifest.csv \
+#     --preprocess_output output/preprocess_output \
+#     --output output2 \
+#     --hg_index data/hg_chr_21_bwa_index.tar.gz \
+#     --distance_threshold 0.1 \
+#     -w work/ \
+#     --noannot \
+#     -resume
 
-# Test with the gene catalog made in a previous round
-NXF_VER=19.10.0 nextflow run main.nf \
-    -c nextflow.config \
-    -profile testing \
-    --gene_fasta output1/ref/genes.fasta.gz \
-    --nopreprocess \
-    --manifest data/mock.manifest.csv \
-    --output output5 \
-    --hg_index data/hg_chr_21_bwa_index.tar.gz \
-    -w work/ \
-    --noannot \
-    -resume
+# # Test with formula and no preprocessing
+# NXF_VER=19.10.0 nextflow run main.nf \
+#     -c nextflow.config \
+#     -profile testing \
+#     --nopreprocess \
+#     --manifest data/mock.manifest.csv \
+#     --output output3 \
+#     --hg_index data/hg_chr_21_bwa_index.tar.gz \
+#     --formula "label1 + label2" \
+#     --distance_threshold 0.1 \
+#     -w work/ \
+#     --noannot \
+#     -resume
+
+# # Test with no formula and no preprocessing
+# NXF_VER=19.10.0 nextflow run main.nf \
+#     -c nextflow.config \
+#     -profile testing \
+#     --nopreprocess \
+#     --manifest data/mock.manifest.csv \
+#     --output output4 \
+#     --hg_index data/hg_chr_21_bwa_index.tar.gz \
+#     -w work/ \
+#     --noannot \
+#     -resume
+
+# # Test with the gene catalog made in a previous round
+# NXF_VER=19.10.0 nextflow run main.nf \
+#     -c nextflow.config \
+#     -profile testing \
+#     --gene_fasta output1/ref/genes.fasta.gz \
+#     --nopreprocess \
+#     --manifest data/mock.manifest.csv \
+#     --output output5 \
+#     --hg_index data/hg_chr_21_bwa_index.tar.gz \
+#     -w work/ \
+#     --noannot \
+#     -resume
 
 
-# Test with the gene catalog made in a previous round and whole genome alignment
-NXF_VER=19.10.0 nextflow run main.nf \
-    -c nextflow.config \
-    -profile testing \
-    --gene_fasta output1/ref/genes.fasta.gz \
-    --nopreprocess \
-    --manifest data/mock.manifest.csv \
-    --output output6 \
-    --hg_index data/hg_chr_21_bwa_index.tar.gz \
-    -w work/ \
-    --eggnog_db false \
-    --eggnog_dmnd false \
-    --taxonomic_dmnd false \
-    --ref_genome_fasta data/reference_genomes.fasta.gz \
-    --ref_genome_csv data/reference_genomes.csv.gz \
-    -resume
+# # Test with the gene catalog made in a previous round and whole genome alignment
+# NXF_VER=19.10.0 nextflow run main.nf \
+#     -c nextflow.config \
+#     -profile testing \
+#     --gene_fasta output1/ref/genes.fasta.gz \
+#     --nopreprocess \
+#     --manifest data/mock.manifest.csv \
+#     --output output6 \
+#     --hg_index data/hg_chr_21_bwa_index.tar.gz \
+#     -w work/ \
+#     --eggnog_db false \
+#     --eggnog_dmnd false \
+#     --taxonomic_dmnd false \
+#     --ref_genome_fasta data/reference_genomes.fasta.gz \
+#     --ref_genome_csv data/reference_genomes.csv.gz \
+#     -resume
 
-# Test with de novo assembly and whole genome alignment
-NXF_VER=19.10.0 nextflow run main.nf \
-    -c nextflow.config \
-    -profile testing \
-    --nopreprocess \
-    --manifest data/mock.manifest.csv \
-    --output output7 \
-    --hg_index data/hg_chr_21_bwa_index.tar.gz \
-    --distance_threshold 0.1 \
-    -w work/ \
-    --eggnog_db false \
-    --eggnog_dmnd false \
-    --taxonomic_dmnd false \
-    --ref_genome_fasta data/reference_genomes.fasta.gz \
-    --ref_genome_csv data/reference_genomes.csv.gz \
-    -resume
+# # Test with de novo assembly and whole genome alignment
+# NXF_VER=19.10.0 nextflow run main.nf \
+#     -c nextflow.config \
+#     -profile testing \
+#     --nopreprocess \
+#     --manifest data/mock.manifest.csv \
+#     --output output7 \
+#     --hg_index data/hg_chr_21_bwa_index.tar.gz \
+#     --distance_threshold 0.1 \
+#     -w work/ \
+#     --eggnog_db false \
+#     --eggnog_dmnd false \
+#     --taxonomic_dmnd false \
+#     --ref_genome_fasta data/reference_genomes.fasta.gz \
+#     --ref_genome_csv data/reference_genomes.csv.gz \
+#     -resume
